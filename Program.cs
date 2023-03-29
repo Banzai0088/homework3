cf38704d7f93437f3977e30983b38ec1afee8253
Alena <banzai0088@ya.ru>
2023-03-29 18:12:32 +0300

    добавили все файлы на отслеживание,проверили задачи

--- Program.cs
+++ Program.cs
@@ -28,54 +28,32 @@
 // }
 // Задача 21: Напишите программу, которая принимает вход в координаты двух точек и нахождение расстояния между ними в 3D-пространствах.
 // А (3,6,8); В (2,1,-7), -> 15,84
-// А (7,-5, 0); В (1,-1,9) -> 11,53
-// int x1 = ReadInt("Введите х для точки 1: ");
-// int y1 = ReadInt("Введите y для точки 1: ");
-// int z1 = ReadInt("Введите z для точки 1: ");
-// int x2 = ReadInt("Введите х для точки 2: ");
-// int y2 = ReadInt("Введите y для точки 2: ");
-// int z2 = ReadInt("Введите z для точки 2: ");
+// // А (7,-5, 0); В (1,-1,9) -> 11,53
+// Console.WriteLine ("Введите х для точки 1: ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите y для точки 1: ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите z для точки 1: ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите х для точки 2: ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите y для точки 2: ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите z для точки 2: ");
+// int zB = Convert.ToInt32(Console.ReadLine());
 
-// int A = x2 - x1;
-// int B = y2 - y1;
-// int C = z2 - z1;
-
-// double length = Math.Sqrt(A * A + B * B + C * C);
-// Console.WriteLine("Расстояние = " + length);
-
-// // Метод
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+// double number = Math.Sqrt((Math.Pow((xA-xB),2))+(Math.Pow((yA-yB),2))+ (Math.Pow((zA-zB),2)));
+// Console.WriteLine("Расстояние = " + number);
 //Задача 23: Напишите программу, которая принимает на вход число (N) и выдает таблетку кубов числами от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125  */
 
-// Console.Write("Введите число: ");
+// Console.WriteLine("Введите число: ");
 // int cube = Convert.ToInt32(Console.ReadLine());
 
-// void Cube(int[] cube){
-//   int counter = 0;
-//   int length = cube.Length;
-//   while (counter <  length){
-//     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-//     counter++;
-//   }
+// for (int count = 1; count <=cube; count++)
+// {
+//     Console.WriteLine (Math.Pow(count,3)+"");
 // }
-
-// void PrintArry(int[] coll){
-//   int count = coll.Length;
-//   int index = 0;
-//   while(index < count){
-//     Console.Write(coll[index]+ " ");
-//     index++;
-//   }
-// } 
-
-// int[] arry = new int[cube+1];
-// Cube(arry);
-// PrintArry(arry);
